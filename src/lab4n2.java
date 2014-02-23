51aaa85eb6946f63cefb333e7463f4a07a661a5c
poriesto <poriesto@hotmail.com>
2014-02-23 19:18:30 +0400

    Initial commit

--- src/lab4n2.java
+++ src/lab4n2.java
@@ -8,74 +8,97 @@
  Использовать ограничения из задания 3.
  Выводить на дисплей результаты работы каждого потока.
  */
+import java.util.Objects;
 import java.util.Random;
-class wrd{
-    protected Object monitor = new Object();
-    protected Random rnd = new Random();
-    private String alphabetSogl = "Б В Г Д Ж З Й К Л М Н П Р С Т Ф Х Ц Ч Ш Щ";
-    private String alphabetGlasn = " А Е Ё И О У Ы Э Ю Я";
-    protected String[] glas, sogl;
-    public String str = " ", final_str = "";
-    private int words_count, word_leghnt;
-    Thread[] threads;
 
-    public void init(int words, int lengt_word){
-        words_count = words;
-        word_leghnt = lengt_word;
-        threads = new Thread[lengt_word];
-        sogl = alphabetSogl.split(" ");
-        glas = alphabetGlasn.split(" ");
-        for(int i = 0; i < glas.length; i ++) glas[i].toLowerCase();
-        for(int j = 0; j < sogl.length; j ++) sogl[j].toLowerCase();
-
+class Char implements Runnable{
+    private static String alphabetSogl = "Б В Г Д Ж З Й К Л М Н П Р С Т Ф Х Ц Ч Ш Щ";
+    private static String alphabetGlasn = " А Е Ё И О У Ы Э Ю Я";
+    protected static String[] sogl = alphabetSogl.split(" ");
+    protected static String[] glas = alphabetGlasn.split(" ");
+    private Random rnd = new Random();
+    private String str = "";
+    private int current;
+    public Char(int c){
+        this.current = c;
     }
-    public void start(){
-        for(int i = 0; i <= words_count -1; i++){
-            System.out.println(i + "\n");
-        for(int j = 0; j <= threads.length-2; j++){
-                threads[j] = new Thread(){
-                    public void run(){
-                        synchronized (monitor){
-                            if((this.getId() + rnd.nextInt() - rnd.nextInt(sogl.length)) % 2 == 0){
-                                str += glas[rnd.nextInt(glas.length)];
-                                System.out.println(this.getName() + " ID = " + this.getId() + " g "+ str);
-                                monitor.notify();
-                            }
-                            else{
-                                str += sogl[rnd.nextInt(sogl.length)];
-                                System.out.println(this.getName() + " ID = " + this.getId() + " s " + str);
-                                monitor.notify();
-                            }
-                        }
-                    }
-                };
-                threads[j+1] = new Thread() {
-                    public void run() {
-                        synchronized (monitor){
-                            System.out.println(getName() + " ID = " + this.getId() +" check " + str);
-                            monitor.notify();
-                        }
-                    }
-                };
-                 System.out.println(j + "\n");
-                threads[j].start();
-                threads[j+1].start();
-                threads[j].interrupt();
-                threads[j+1].interrupt();
+    public void run(){
+        if(current % 2 == 0){
+            System.out.print(glas[rnd.nextInt(glas.length-1)]);
+        }
+        else{
+            System.out.print(sogl[rnd.nextInt(sogl.length-1)]);
+        }
+    }
+}
+class CheckChar implements Runnable{
+    private int current, last;
+    public CheckChar(int i, int c){
+        this.current = i;
+        this.last = c;
+    }
+    public void run(){
+        if(this.current == this.last){
+            System.out.print("@");
+        }
+    }
+}
+class CheckWord implements Runnable{
+    private int current, last;
+    public CheckWord(int c, int l){
+        this.current = c;
+        this.last = l;
+    }
+    public void run(){
+        if(this.current == this.last){
+            System.out.print(".");
+        }
+    }
+}
+class Word implements Runnable{
+    int c;
+    private Object monitor = new Object();
+    public Word(int count){
+        this.c = count;
+    }
+    public void run(){
+        for(int i = 0; i <= this.c; i++){
+            Runnable wr = new Char(i);
+            Runnable chk = new CheckChar(i, this.c);
+            synchronized (this.monitor){
+                new Thread(wr).start();
             }
-            str += " ";
-     }
-        str+= ".";
+            synchronized (this.monitor){
+                new Thread(chk).start();
+            }
+        }
+    }
+}
+class Words implements Runnable{
+    private int last, chars;
+    private Object monitor = new Object();
+
+    public Words(int l, int ch){
+        this.last = l;
+        this.chars = ch;
+    }
+    public void run(){
+        Runnable[] words = new Word[this.last];
+        for(int i = 0; i < words.length; i++){
+            words[i] = new Word(this.chars);
+            Runnable checker = new CheckWord(i, this.last);
+            synchronized (monitor){
+                new Thread(words[i]).start();
+            }
+            synchronized (monitor){
+                new Thread(checker).start();
+            }
+        }
     }
 }
 
-public class lab4n2 {
-
-    public static void main(String argv[]) throws InterruptedException{
-        wrd words = new wrd();
-        words.init(Integer.parseInt(argv[0]), Integer.parseInt(argv[1]));
-        words.start();
-        System.out.println("Result of program:\n" + words.str.toLowerCase());
+public class lab4n2{
+    public static void main(String argv[]){
     }
 
 }
